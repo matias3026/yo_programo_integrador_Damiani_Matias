c003383e034b22ceff1d136b72d8204d34ad92c3
matias <matias3026@gmail.com>
2022-09-06 14:23:41 -0300

    se creo todo de nuevo ,malditos puertos

--- yo_programo_integrador_Damiani_Matias/src/app/app.module.ts
+++ yo_programo_integrador_Damiani_Matias/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    LogoAPComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
