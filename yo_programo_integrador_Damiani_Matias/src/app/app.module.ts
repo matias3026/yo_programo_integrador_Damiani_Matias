d3421409e1ed41c8595aa66c6e5fe2242f233d08
matias <matias3026@gmail.com>
2022-09-07 13:53:52 -0300

    Hard & Soft skills

--- yo_programo_integrador_Damiani_Matias/src/app/app.module.ts
+++ yo_programo_integrador_Damiani_Matias/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HysComponent } from './components/hys/hys.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
-    EducacionComponent
+    EducacionComponent,
+    HysComponent
   ],
   imports: [
     BrowserModule,
